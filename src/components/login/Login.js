82c4bdd4c2591482ef47ca56c598bc33046c74e7
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-02-19 12:41:02 +0530

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./login.css"
+import "./login.css";
 import { useNavigate, Link } from 'react-router-dom';
 import axios from "axios"
 import { toast } from "react-toastify"
