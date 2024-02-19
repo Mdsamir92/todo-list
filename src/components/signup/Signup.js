e4beb3ba82a796e668e883b643db9199b6f20610
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-02-19 12:25:09 +0530

    Update Signup.js

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios"
-import "./signup.css";
-import {useNavigate ,Link} from 'react-router-dom';
-import {toast} from "react-toastify"
+import "./Signup.css";
+import { useNavigate, Link } from 'react-router-dom';
+import { toast } from "react-toastify"
 
 
 function Signup() {
@@ -13,82 +13,75 @@ function Signup() {
   // const [error, setError] = useState("");
 
 
-const navigate = useNavigate("");
+  const navigate = useNavigate("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
- 
+
     if (!name || !email || !password) {
       toast.error("Enter valid name, email & password 🙁");
       return;
-   }
-  else if(name.length<3){
-     toast.error("Name requires a minimum of 3 characters");
-     return;
-   }else if (email.length < 13) { 
-     toast.error("Enter a valid email address");
-     return;
-   }
-   else if (password.length < 5) {
-     toast.error("Password requires a minimum of 5 characters");
-     return ;
-   }
-  
-
-  
-   axios.post("https://login-register-form-go9w.onrender.com/register", 
-   { name,email,password }
-
-  ).then(() => {
-    toast.success("Register successfully😍")
-    navigate("/");
-  })
-   
-  .catch(() => {
-     
-    if (!name || !email || !password) {
-      toast.error("Enter valid name, email & password 🙁");
+    }
+    else if (name.length < 3) {
+      toast.error("Name requires a minimum of 3 characters");
       return;
-   }
-   
-   else
-     {
-      toast.error("Email already registered 🙁");
-      return ;
-     }
-})
+    } else if (email.length < 13) {
+      toast.error("Enter a valid email address");
+      return;
+    }
+    else if (password.length < 5) {
+      toast.error("Password requires a minimum of 5 characters");
+      return;
+    }
 
 
 
-}
+    axios.post("https://login-register-form-go9w.onrender.com/register",
+      { name, email, password }
+
+    ).then(() => {
+      toast.success("Register successfully😍")
+      navigate("/");
+    })
+
+      .catch(() => {
+
+        if (!name || !email || !password) {
+          toast.error("Enter valid name, email & password 🙁");
+          return;
+        }
+
+        else {
+          toast.error("Email already registered 🙁");
+          return;
+        }
+      })
+
+
+
+  }
 
 
   return (
     <div className='form-container'>
-   <h2>SignUp</h2>
+      <h2>Register🖥</h2>
 
-   <div class="social-icons">
-        <a href="#" ><i class="fa-brands fa-google"></i> </a>
-        <a href="#" ><i class="fa-brands fa-facebook"></i> </a>
-        <a href="#" ><i class="fa-brands fa-github"></i> </a>
-    
-      </div>
+
       <form>
         <label>Name:</label> <br />
-        <input type="text" value={name} placeholder="enter name" onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={name}  placeholder="enter name" onChange={(e) => setName(e.target.value)} />
         <br />
         <label>Email:</label> <br />
-        <input type="email" value={email} placeholder="enter email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="email" value={email}  placeholder="enter email" onChange={(e) => setEmail(e.target.value)} />
         <br />
         <label>Password:</label> <br />
-        <input type="password" value={password} placeholder="enter password" onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" value={password}   placeholder="enter password" onChange={(e) => setPassword(e.target.value)} />
         <br />
-        {/* <p style={{ color: "red" }}>{error && error}</p> */}
-       
-       <button type="submit" onClick={handleSubmit} className='reg-btn'>Register</button>
-     
+
+        <button type="submit" onClick={handleSubmit} className='reg-btn'>Register</button>
+
       </form>
-      <br />
+  
       <Link to="/">Login with existing account</Link>
     </div>
   )
